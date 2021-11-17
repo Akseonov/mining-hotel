4f6287ef0c15748d3e4edf59329d59a2d0cb8c90
Malcador <malcadorssigil@gmail.com>
2021-11-17 11:13:46 +0500

    MH-1  - удалил prettier из-за конфликтов с space-in-parens  - настроил eslintrc.js

--- vue.config.js
+++ vue.config.js
@@ -1,70 +1,49 @@
-// const path = require( 'path' );
+const path = require( "path" );
 
 const { NODE_ENV, HOST, PORT } = process.env;
 const isDev = NODE_ENV === "development";
-const mode = isDev ? "development" : "production";
+// const mode = isDev ? "development" : "production";
 const host = HOST || "localhost";
 const port = PORT || 3000;
 
-// const paths = {
-// 	src: path.resolve( __dirname, 'src' ),
-// 	html: path.resolve( __dirname, 'src', 'assets', 'html' ),
-// 	build: path.resolve( __dirname, 'build' ),
-// };
+const paths = {
+	src: path.resolve( __dirname, "src" ),
+	html: path.resolve( __dirname, "src", "assets", "html" ),
+	build: path.resolve( __dirname, "build" ),
+};
 
 module.exports = {
-  // mode,
-  // entry: {
-  // 	'legacy-main': './src/index.js',
-  // },
-  // output: {
-  // 	filename: '[name].js',
-  // 	path: paths.build,
-  // 	// publicPath: '/', // криво работает с build
-  // 	// clean: true, // не работает вместе с devServer writeToDisk, баг не устранен на 27.09.2021, поставил clean-webpack-plugin
-  // },
-  // stats: {
-  // 	assets: true,
-  // 	modules: false,
-  // },
-  // target: process.env.NODE_ENV === "development" ? "web" : "browserslist",
-  // devtool: isDev
-  // 	? 'eval-cheap-module-source-map'
-  // 	: false,
-  devServer: {
-    // static: {
-    // 	publicPath: '/',
-    // },
-    // devMiddleware: {
-    // 	index: true,
-    // 	writeToDisk: true,
-    // 	publicPath: '/',
-    // 	stats: 'minimal',
-    // },
-    // client: {
-    // 	overlay: {
-    // 		errors: true,
-    // 		warnings: true,
-    // 	},
-    // },
-    hot: false,
-    host: "localhost",
-    port: 3000,
-    writeToDisk: true,
-    overlay: {
-      errors: true,
-      warnings: true,
-    },
-    stats: "errors-only",
-    // host,
-    // port,
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        pathRewrite: {
-          "^/api": "",
-        },
-      },
-    },
-  },
+	// mode,
+	publicPath: "/",
+	outputDir: paths.build,
+	lintOnSave: isDev,
+	productionSourceMap: false,
+
+	// stats: {
+	// 	assets: true,
+	// 	modules: false,
+	// },
+	// target: 'web',
+	// devtool: isDev
+	// 	? 'eval-cheap-module-source-map'
+	// 	: 'none',
+	devServer: {
+		hot: false,
+		writeToDisk: true,
+		overlay: {
+			errors: true,
+			warnings: true,
+		},
+		stats: "errors-only",
+		host,
+		port,
+		proxy: {
+			"/api": {
+				target: "http://localhost:4000",
+				pathRewrite: {
+					"^/api": "",
+				},
+			},
+		},
+	},
 };
