7613bf8adb21ef0855c1997dc751dfcb6485f553
akseonov <konstantinaxeonov@gmail.com>
2022-03-20 20:06:56 +0500

    MH-1-vuetify  - изменения по баннерам

--- src/components/TheBanners.vue
+++ src/components/TheBanners.vue
@@ -1,18 +1,27 @@
 <template>
 	<v-container class="px-4 py-0 mt-5 d-flex banners__gap">
-		<v-row class="ma-0 banners__gap">
-			<v-col class="bg-base-1dp rounded-4 d-flex flex-column justify-end">
-				<svg v-svg symbol="miner" size="49"></svg>
-				<h3 class="text-base-86 text-subtitle-2">Купите свой первый майнер</h3>
+		<v-row class="ma-0 banners__gap banners__row flex-column">
+			<v-col class="pa-0 bg-base-1dp rounded-4 d-flex flex-column justify-end banners__col">
+				<div>
+					<div class="pa-4 d-flex flex-column justify-space-between">
+						<svg v-svg symbol="miner" size="49"></svg>
+						<h3 class="text-base-86 text-subtitle-2">Купите свой первый майнер</h3>
+					</div>
+				</div>
 			</v-col>
-			<v-col class="bg-yellow-300 rounded-t-4 rounded-r-4 d-flex flex-column justify-end">
+			<v-col class="pa-0 bg-yellow-300 rounded-t-4 rounded-r-4 d-flex flex-column justify-end
+			banners__col">
 <!--				<svg v-svg symbol="clock" size="20"></svg>-->
-				<v-img src="/img/icons/calc.svg" width="130" height="70" class="mx-auto"  />
-				<h3 class="text-base-black text-subtitle-2">Калькулятор доходности </h3>
+				<div>
+					<div class="py-4 d-flex flex-column justify-space-between">
+						<v-img src="/img/icons/calc.svg" width="130" height="70" class="mx-auto"  />
+						<h3 class="px-4 text-base-black text-subtitle-2">Калькулятор доходности </h3>
+					</div>
+				</div>
 			</v-col>
 		</v-row>
-		<v-row class="ma-0">
-			<v-col class="bg-base-1dp rounded-4 d-flex flex-column justify-end">
+		<v-row class="ma-0 banners__row">
+			<v-col class="pa-4 bg-base-1dp rounded-4 d-flex flex-column justify-end">
 				<svg v-svg symbol="clock" size="20"></svg>
 				<svg v-svg symbol="clock" size="20"></svg>
 				<h3 class="text-base-86 text-subtitle-2 mb-4">Майнинг отель</h3>
@@ -60,6 +69,28 @@ export default {
 .banners {
 	&__gap {
 		gap: 16px
+	}
+
+	&__row {
+		width: 50%;
+	}
+
+	&__col {
+		width: 100%;
+
+		& > div {
+			position: relative;
+			padding-bottom: 100%;
+			height: 0;
+
+			& > div {
+				position: absolute;
+				left: 0;
+				top: 0;
+				height: 100%;
+				width: 100%;
+			}
+		}
 	}
 }
 </style>
--- src/components/TheExchangeRates.vue
+++ src/components/TheExchangeRates.vue
@@ -1,21 +1,21 @@
 <template>
-	<v-container class="pa-0 pl-4 mt-5">
+	<v-container class="pa-0 mt-5">
 		<div class="d-flex" ref="slider">
-			<div class="">
+			<div class="pl-4">
 				<p class="text-base-86 text-caption-2">BTC/USD</p>
 				<div class="d-flex">
 					<p class="text-base-60 text-caption-2 d-flex align-center mr-1 text-no-wrap">58 834</p>
 					<p class="text-green-300 text-caption-2 pb-1 text-no-wrap">+693 (1,19%)</p>
 				</div>
 			</div>
-			<div class="">
+			<div>
 				<p class="text-base-86 text-caption-2">ETH/USD</p>
 				<div class="d-flex">
 					<p class="text-base-60 text-caption-2 d-flex align-center mr-1 text-no-wrap">2.100,59</p>
 					<p class="text-green-300 text-caption-2 pb-1 text-no-wrap">+21,05 (1,12%)</p>
 				</div>
 			</div>
-			<div class="">
+			<div>
 				<p class="text-base-86 text-caption-2">ZEC/USD</p>
 				<div class="d-flex">
 					<p class="text-base-60 text-caption-2 d-flex align-center mr-1 text-no-wrap">193,07</p>
